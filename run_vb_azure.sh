fc4422bae54084369d1167158f48e790d84f3172
maodaye.zheng@gmail.com <maodaye.zheng@gmail.com>
2017-07-26 22:29:20 +0100

    update run script

--- run_vb_azure.sh
+++ run_vb_azure.sh
@@ -5,12 +5,6 @@
 #$ -l gpu=1,gpu_P100_16=1
 #!/bin/sh
 
-OUT_DIR=code_outputs/final_model
+mkdir code_outputs/final_model
 
-mkdir ${OUT_DIR}
-
-PYTHON_FILE=translation_experiment.py
-
-cp ${PYTHON_FILE} ${OUT_DIR}/${PYTHON_FILE}
-
-THEANO_FLAGS=mode=Mode,device=cuda0,floatX=float32 python -u ${PYTHON_FILE} ${OUT_DIR} | tee ${OUT_DIR}/out.txt
+THEANO_FLAGS=mode=Mode,device=cuda0,floatX=float32 python -u translation_experiment.py code_outputs/final_model | tee code_outputs/final_model/out.txt
