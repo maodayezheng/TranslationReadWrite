2fd05dd432a77b5842c70375ad57074a1b2064ec
maodaye.zheng@gmail.com <maodaye.zheng@gmail.com>
2017-07-27 18:47:25 +0100

    Start tweaking the model

--- ModelUtils/ParamFreeVAE/Translation/DeepReluRW.py
+++ ModelUtils/ParamFreeVAE/Translation/DeepReluRW.py
@@ -30,13 +30,13 @@ random = MRG_RandomStreams(seed=1234)
 
 class DeepReluTransReadWrite(object):
     def __init__(self, training_batch_size=25, source_vocab_size=37007, target_vocab_size=37007,
-                 embed_dim=512, hid_dim=1024, source_seq_len=50, target_seq_len=50):
+                 embed_dim=4, hid_dim=8, source_seq_len=50, target_seq_len=50):
         self.source_vocab_size = source_vocab_size
         self.target_vocab_size = target_vocab_size
         self.batch_size = training_batch_size
         self.hid_size = hid_dim
         self.max_len = 51
-        self.output_score_dim = 512
+        self.output_score_dim = 4
         self.embedding_dim = embed_dim
 
         # Init the word embeddings.
@@ -673,6 +673,7 @@ def decode():
 
 def run(out_dir):
     print("Run the Relu read and  write final model ")
+    print("param_save at " + out_dir)
     training_loss = []
     validation_loss = []
     model = DeepReluTransReadWrite()
@@ -732,7 +733,7 @@ def run(out_dir):
     print(" The training data size : " + str(data_size))
     batch_size = 50
     sample_groups = 10
-    iters = 60000
+    iters = 2
     print(" The number of iterations : " + str(iters))
 
     for i in range(iters):
@@ -784,7 +785,7 @@ def run(out_dir):
             loss = output[0]
             training_loss.append(loss)
 
-            if i % 250 == 0:
+            if i % 25 == 0:
                 print("training time " + str(iter_time)
                       + " sec with sentence length " + str(l)
                       + " training loss : " + str(loss))
--- translation_experiment.py
+++ translation_experiment.py
@@ -1,14 +1,12 @@
 from ModelUtils.ParamFreeVAE.Translation.DeepReluRW import run
-
 import sys
 import numpy as np
-from theano.sandbox.rng_mrg import MRG_RandomStreams as RandomStreams
 
 sys.setrecursionlimit(5000000)
 np.set_printoptions(threshold=1000000)
-out_dir = sys.argv[1]
-srng = RandomStreams(seed=1234)
+main_dir = sys.argv[0]
+out_dir = sys.argv[2]
 
 if __name__ == '__main__':
+    print("Start run trainslation experiment")
     run(out_dir)
-
