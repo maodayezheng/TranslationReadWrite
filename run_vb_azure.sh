f3106a3e6d1d26ee85bd783737c8daae1601c90f
maodaye.zheng@gmail.com <maodaye.zheng@gmail.com>
2017-07-27 12:53:31 +0100

    update run script

--- run_vb_azure.sh
+++ run_vb_azure.sh
@@ -7,4 +7,20 @@
 #$ -j y
 #$ -N FinalModel
 
-THEANO_FLAGS=mode=Mode,device=cuda0,floatX=float32 python -u translation_experiment.py code_outputs/final_model | tee code_outputs/final_model/out.txt
+SOURCE="${BASH_SOURCE[0]}"
+while [ -h "${SOURCE}" ]; do
+  DIR="$( cd -P "$( dirname "${SOURCE}" )" && pwd )"
+  SOURCE="$(readlink "${SOURCE}")"
+  [[ ${SOURCE} != /* ]] && SOURCE="${DIR}/${SOURCE}"
+done
+DIR="$( cd -P "$( dirname "${SOURCE}" )" && pwd )"
+
+OUT_DIR=code_outputs/$(date +%Y_%m_%d_%H_%M_%S)
+
+mkdir ${OUT_DIR}
+
+PYTHON_FILE=${1}
+
+cp ${PYTHON_FILE} ${OUT_DIR}/${PYTHON_FILE}
+
+THEANO_FLAGS=mode=Mode,device=cuda0,floatX=float32 python -u ${PYTHON_FILE} ${DIR} ${OUT_DIR} | tee ${OUT_DIR}/out.txt
