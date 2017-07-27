c7d4c2ae5c05c23951a3616d6d40aa5e0829b162
maodaye.zheng@gmail.com <maodaye.zheng@gmail.com>
2017-07-27 12:50:57 +0100

    update azure script

--- run_vb_azure.sh
+++ run_vb_azure.sh
@@ -3,6 +3,8 @@
 #$ -l h_vmem=15G
 #$ -P gpu
 #$ -l gpu=1,gpu_P100_16=1
-#!/bin/sh
+#$ -S /bin/bash
+#$ -j y
+#$ -N FinalModel
 
 THEANO_FLAGS=mode=Mode,device=cuda0,floatX=float32 python -u translation_experiment.py code_outputs/final_model | tee code_outputs/final_model/out.txt
