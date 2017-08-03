813593c05f6648247acbe60893660f728c9d9ca3
maodaye.zheng@gmail.com <maodaye.zheng@gmail.com>
2017-08-03 14:24:08 +0100

    remove redundant code

--- ModelUtils/ParamFreeVAE/Translation/DeepReluRW.py
+++ ModelUtils/ParamFreeVAE/Translation/DeepReluRW.py
@@ -30,7 +30,7 @@ random = MRG_RandomStreams(seed=1234)
 
 class DeepReluTransReadWrite(object):
     def __init__(self, training_batch_size=25, source_vocab_size=37007, target_vocab_size=37007,
-                 embed_dim=512, hid_dim=512, source_seq_len=50, target_seq_len=50):
+                 embed_dim=512, hid_dim=512):
         self.source_vocab_size = source_vocab_size
         self.target_vocab_size = target_vocab_size
         self.batch_size = training_batch_size
