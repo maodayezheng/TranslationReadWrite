0a2d24f72e2f9da7ed5385b40fb569055e3356ae
maodaye.zheng@gmail.com <maodaye.zheng@gmail.com>
2017-08-15 13:50:18 +0100

    update Deep Relu Bow Read for training

--- translation_experiment.py
+++ translation_experiment.py
@@ -1,4 +1,4 @@
-from ModelUtils.ParamFreeVAE.Translation.Seq2SeqAttention import run
+from ModelUtils.ParamFreeVAE.Translation.DeepReluSimpleBowReadRNN import run
 import sys
 import numpy as np
 
--- val_train_compare.py
+++ val_train_compare.py
@@ -1,19 +1,6 @@
 import numpy as np
 import matplotlib.pyplot as plt
 
-folder = "code_outputs/"
-date = "2017_08_09_14_52_53/"
-loss = "validation_loss.npy"
-valid_loss = np.load(folder+date+loss)
-print(valid_loss[:-1].shape)
-#attn_loss = np.mean(attn_loss[:-1].reshape((6, 25000)), axis=-1)
-
-plt.plot(-valid_loss, label='valid_loss', zorder=3)
-plt.xlabel('Negative log likelihood (Averaged over 1000 iterations)')
-plt.ylabel('L(X))')
-plt.legend(loc=2)
-plt.tight_layout()
-plt.show()
 """
 total_elbos =0
 for i in range(len(training)):
