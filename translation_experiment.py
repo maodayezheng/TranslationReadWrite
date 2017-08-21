2884668996b96557c1289906742ee788e62ebb9a
maodaye.zheng@gmail.com <maodaye.zheng@gmail.com>
2017-08-21 14:57:48 +0100

    update the training script

--- translation_experiment.py
+++ translation_experiment.py
@@ -1,14 +1,159 @@
-from ModelUtils.ParamFreeVAE.Translation.DeepReluIORNNSearchV4 import run
+from ModelUtils.ParamFreeVAE.Translation.DeepReluIORNNSearchV4 import DeepReluTransReadWrite
 from ModelUtils.ParamFreeVAE.DynamicLayers.DynamicVallina import run as dynamic_run
 from ModelUtils.ParamFreeVAE.DeepReluIORNN.FourLayersV2 import run as deep_io_run
 import sys
+import time
+import json
+import lasagne
+import os
+import pickle as cPickle
 import numpy as np
 
 sys.setrecursionlimit(5000000)
 np.set_printoptions(threshold=1000000)
 main_dir = sys.argv[0]
 out_dir = sys.argv[2]
+batch_size = 25
+sample_groups = 10
+iters = 60000
+pre_trained = False
+restore_date = ""
+restore_params = ""
+training_data_file = "BPE/train50.tok.bpe.32000.txt"
+show_address = True
 
 if __name__ == '__main__':
-    print("Start run trainslation experiment")
-    deep_io_run(out_dir)
+    print("Run the Relu IO read 4 layers RNN")
+    print("Params saved at " + out_dir)
+    training_loss = []
+    validation_loss = []
+    model = DeepReluTransReadWrite()
+    if pre_trained:
+        with open("code_outputs/" + restore_date + restore_params, "rb") as params:
+            print("Params restored from 2017_08_12_21_35_35")
+            model.set_param_values(cPickle.load(params))
+    update_kwargs = {'learning_rate': 1e-4}
+    draw_sample = False
+    optimiser, updates = model.optimiser(lasagne.updates.adam, update_kwargs, draw_sample)
+    validation = model.elbo_fn()
+    train_data = None
+
+    with open("SentenceData/" + training_data_file, "r") as dataset:
+        train_data = json.loads(dataset.read())
+
+    validation_data = None
+    with open("SentenceData/BPE/news2013.tok.bpe.32000.txt", "r") as dev:
+        validation_data = json.loads(dev.read())
+
+    validation_data = sorted(validation_data, key=lambda d: max(len(d[0]), len(d[1])))
+    len_valid = len(validation_data)
+    splits = len_valid % batch_size
+    validation_data = validation_data[:-splits]
+    validation_data = np.array(validation_data)
+    print(" The chosen validation size : " + str(len(validation_data)))
+    g = int(len(validation_data) / batch_size)
+    print(" The chosen validation groups : " + str(g))
+    validation_data = np.split(validation_data, g)
+
+    validation_pair = []
+    for m in validation_data:
+        l = max(len(m[-1, 0]), len(m[-1, 1]))
+        start = time.clock()
+        source = None
+        target = None
+        for datapoint in m:
+            s = np.array(datapoint[0])
+            t = np.array(datapoint[1])
+            if len(s) != l:
+                s = np.append(s, [-1] * (l - len(s)))
+            if len(t) != l:
+                t = np.append(t, [-1] * (l - len(t)))
+            if source is None:
+                source = s.reshape((1, s.shape[0]))
+            else:
+                source = np.concatenate([source, s.reshape((1, s.shape[0]))])
+            if target is None:
+                target = t.reshape((1, t.shape[0]))
+            else:
+                target = np.concatenate([target, t.reshape((1, t.shape[0]))])
+
+        validation_pair.append([source, target])
+
+    # calculate required iterations
+    data_size = len(train_data)
+    print(" The training data size : " + str(data_size))
+    print(" The number of iterations : " + str(iters))
+
+    for i in range(iters):
+        batch_indices = np.random.choice(len(train_data), batch_size * sample_groups, replace=False)
+        mini_batch = [train_data[ind] for ind in batch_indices]
+        mini_batch = sorted(mini_batch, key=lambda d: max(len(d[0]), len(d[1])))
+        samples = None
+
+        mini_batch = np.array(mini_batch)
+        mini_batchs = np.split(mini_batch, sample_groups)
+        loss = None
+        read_attention = None
+        write_attention = None
+        for m in mini_batchs:
+            l = max(len(m[-1, 0]), len(m[-1, 1]))
+            source = None
+            target = None
+            start = time.clock()
+            for datapoint in m:
+                s = np.array(datapoint[0])
+                t = np.array(datapoint[1])
+                if len(s) != l:
+                    s = np.append(s, [-1] * (l - len(s)))
+                if len(t) != l:
+                    t = np.append(t, [-1] * (l - len(t)))
+                if source is None:
+                    source = s.reshape((1, s.shape[0]))
+                else:
+                    source = np.concatenate([source, s.reshape((1, s.shape[0]))])
+                if target is None:
+                    target = t.reshape((1, t.shape[0]))
+                else:
+                    target = np.concatenate([target, t.reshape((1, t.shape[0]))])
+
+            output = optimiser(source, target)
+            iter_time = time.clock() - start
+            loss = output[0]
+            training_loss.append(loss)
+
+            if i % 1000 == 0:
+                print("training time " + str(iter_time) + " sec with sentence length " + str(l)
+                      + " training loss : " + str(loss))
+
+        if i % 500 == 0:
+            valid_loss = 0
+            p = 0
+            v_out = None
+            for pair in validation_pair:
+                p += 1
+                v_out = validation(pair[0], pair[1])
+                valid_loss += v_out[0]
+
+            print("The loss on testing set is : " + str(valid_loss / p))
+            validation_loss.append(valid_loss / p)
+            if i % 10000 == 0 and show_address:
+                v_r = v_out[1]
+                for n in range(1):
+                    for t in range(v_r.shape[0]):
+                        print("======")
+                        print(" Source " + str(v_r[t, n]))
+                        print("")
+
+        if i % 2000 == 0 and i is not 0:
+            print("Params saved at iteration " + str(i))
+            np.save(os.path.join(out_dir, 'training_loss.npy'), training_loss)
+            np.save(os.path.join(out_dir, 'validation_loss'), validation_loss)
+            with open(os.path.join(out_dir, 'model_params.save'), 'wb') as f:
+                cPickle.dump(model.get_param_values(), f, protocol=cPickle.HIGHEST_PROTOCOL)
+                f.close()
+
+    np.save(os.path.join(out_dir, 'training_loss.npy'), training_loss)
+    np.save(os.path.join(out_dir, 'validation_loss.npy'), validation_loss)
+    with open(os.path.join(out_dir, 'final_model_params.save'), 'wb') as f:
+        cPickle.dump(model.get_param_values(), f, protocol=cPickle.HIGHEST_PROTOCOL)
+        f.close()
