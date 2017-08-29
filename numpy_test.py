4f4b5cdbfbdedbaaebe09f75fc4daf32144fd8b8
maodaye.zheng@gmail.com <maodaye.zheng@gmail.com>
2017-08-29 21:34:14 +0100

    update attention frac

--- numpy_test.py
+++ numpy_test.py
@@ -1,2 +1,10 @@
 import numpy as np
 
+l = 10
+times = np.array([1, 5, 6]) * 0.5
+step = np.arange(10*0.5)
+l = step.shape[0]
+step = step.reshape((1, l))
+t = times.shape[0]
+times = times.reshape((t, 1))
+print(np.less(step, times))
